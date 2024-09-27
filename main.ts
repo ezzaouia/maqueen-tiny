2fb047eabf4061c2a4d593a4a39d5e5600cf438a
Mohamed Ezzaouia <ezzaouia@users.noreply.github.com>
2024-09-27 07:19:59 -0700

    fixing tank left and right

--- main.ts
+++ main.ts
@@ -298,11 +298,13 @@ namespace tiny {
                 maqueen.motorStop(maqueen.Motors.All)
                 break;
             }
+            case Direction.Left:
             case Direction.SpinLeft: {
                 maqueen.motorStop(maqueen.Motors.M1)
                 maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed2)
                 break;
             }
+            case Direction.Right:
             case Direction.SpinRight: {
                 maqueen.motorStop(maqueen.Motors.M2)
                 maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed1)
